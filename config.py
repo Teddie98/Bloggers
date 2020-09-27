cf8bf4520f62fd472110118f6b3c44c5c2ad973c
Teddie <teddymachoka98@yahoo.com>
2020-09-27 10:20:00 +0300

    add sll the folders created

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # configure the path to where image files will be saved since it is not advisable to save files in the database
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    pass
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vikki:sasawa@localhost/blogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG = True
+    pass
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
+    pass
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
---
+++ manage.py
@@ -0,0 +1,24 @@
+from app import create_app
+from flask_script import Manager, Server
+from app import db
+from app.models import User, BlogPost, Image, Comment
+from flask_migrate import Migrate, MigrateCommand
+
+# import the create app funtion from app and create an instance of the entire app while passing in the cofiguration settings in the parameters of the funtion
+app = create_app('production')
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+# add command for running the server using manager
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, BlogPost=BlogPost, Image=Image, Comment=Comment)
+
+
+if __name__ == '__main__':
+    manager.run()
